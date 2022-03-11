b817b6d91bd8f715b9f37f819005170d7ff7b968
alex <fxj5060@buaa.edu.cn>
2022-03-11 14:12:25 +0800

    修改了assetsPublicPath

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,1 @@
-// module.exports = { outputDir: 'dist', publicPath: process.env.NODE_ENV === 'production' ? '/fxj_demo_app/' : '/' }
-module.exports = {
-    outputDir: 'dist',
-    publicPath: '/fxj_demo_app/'
-}
+module.exports = { outputDir: 'dist', publicPath: process.env.NODE_ENV === 'production' ? '/fxj_demo_app/' : '/' }
