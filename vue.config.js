36cc45d41d8ae819501f815cfa4228ac1688e31b
alex <fxj5060@buaa.edu.cn>
2022-03-11 13:10:50 +0800

    debug 4

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,5 @@
-module.exports = { outputDir: 'dist', publicPath: process.env.NODE_ENV === 'production' ? '/fxj_demo_app/' : '/' }
+// module.exports = { outputDir: 'dist', publicPath: process.env.NODE_ENV === 'production' ? '/fxj_demo_app/' : '/' }
+module.exports = {
+    outputDir: 'dist',
+    publicPath: '/'
+}
